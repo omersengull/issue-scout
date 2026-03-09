9325d02af7a2115e10607509037cbfb71f6d6a67
omersengull <omersengul061@hotmail.com>
2026-03-10 01:15:23 +0300

    fix: update package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import { reactRouter } from "@react-router/dev/vite";
-import { cloudflareDevProxy } from "@react-router/adapter-cloudflare";
+import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    cloudflareDevProxy(), // Bu satırın olduğundan emin ol
+    cloudflareDevProxy(),
     reactRouter(),
     tailwindcss(),
     tsconfigPaths(),
